cf113634d38e64304ea2e56f61370c5d16234f96
ShaipZekolli <98315960+ShaipZekolli@users.noreply.github.com>
2022-04-16 20:12:13 +0200

    Update Normailizimi_Al.py

--- Normailizimi_Al.py
+++ Normailizimi_Al.py
@@ -1,7 +1,11 @@
+#ShZ
+import sys
 import re
 import num2words
 from deep_translator import GoogleTranslator
-import sys
+import gtts
+
+#Konvertimi i numrave ne fjale
 try:
     text=""
     input = sys.argv[1]
@@ -11,9 +15,14 @@ except:
     print("Jepni një file si input.\np.sh 'python Normalizimi AL.py shembull.txt'")
 text = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), text)
 
+#Perkthimi nga english ne shqip
 with open('output.txt', 'w') as f_output:
     f_output.write(text)
 translated = GoogleTranslator(source='english', target='albanian').translate_file('output.txt')
+
+#Gjenerimi i .mp3 fiajllit nga teksti
 with open('output.txt', 'w') as f_output:
     f_output.write(translated)
-print(translated)
+    tts = gtts.gTTS(translated, lang="sq" , tld='al',slow=True )
+    tts.save("output.mp3")
+print(translated
